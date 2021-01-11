c7110651f1872a5ee5fe0ac6ef88e47f58ce342b
Jonathan Hatton <56257755+JEGHatton@users.noreply.github.com>
2021-01-11 14:17:02 +0000

    fixing logout button
    added some bootstrap and script files since my npm doesn't want to load the correct files for the default laravel css to function. user page header drop down now works.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
     <!-- CSS only -->
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <div class="container">
         <div class="row justify-content-center">
